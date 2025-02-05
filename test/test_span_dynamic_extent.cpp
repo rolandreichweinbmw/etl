53b3752cc1b2a8bdeb2add55e26a331bf4a03f78
Roland Reichwein <Roland.Reichwein@bmw.de>
2025-02-05 14:06:27 +0100

    Guard advance()

--- test/test_span_dynamic_extent.cpp
+++ test/test_span_dynamic_extent.cpp
@@ -1242,6 +1242,10 @@ namespace
         CHECK_EQUAL(data0[0], 0x05);
         data0.advance(1);
         CHECK_EQUAL(data0.size(), 0);
+        data0.advance(1);
+        CHECK_EQUAL(data0.size(), 0);
+        data0.advance(100);
+        CHECK_EQUAL(data0.size(), 0);
       }
     }
 
